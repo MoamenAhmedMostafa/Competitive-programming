2435e79041f3a81f07f050e4ef99621496c07f81
MoamenAhmedMostafa <36875788+MoamenAhmedMostafa@users.noreply.github.com>
2018-03-19 13:13:09 -0700

    Add files via upload

---
+++ UVA/UVA  10340 .cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v)          ((v).begin()), ((v).end())
+#define pb push_back
+#define endl '\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define clr(v,d)		memset(v, d, sizeof(v))
+const double EPS = 1e-12;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = ll(1e9 + 7);
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+void stop(){
+#ifndef ONLINE_JUDGE
+	system("pause");
+#endif
+}
+void run(){
+#ifndef ONLINE_JUDGE
+	freopen("intput.txt", "r", stdin);
+#endif
+
+}
+int main() {
+	fast
+		string a, b;
+	while (cin >> a >> b){
+		int e = 0;
+		for (auto i : b)if (i == a[e])e++;
+		if (e == a.size())cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
+	stop();
+	return 0;
+}
